7d01572690880c92cf21b66696f5d34da629f9b7
Chirag Anand <chiraganand@live.com>
2023-02-11 21:30:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(4+5) //we are using node.js not browser.
+
+console.log(3
+    +
+    3)  //Code readability should be high priority.
+
+console.log("anand");
+
+let name = "chirag"
+let age = 15
+let isLoggedIn = true
+
+// Primitive Datatypes
+// number => 2 to power 53
+// bigint
+// string => "string"
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "chirag");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
