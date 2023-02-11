b75276dc57a51594d9ce23de5f03640e0bce2c8b
Chirag Anand <chiraganand@live.com>
2023-02-11 22:02:29 +0000

    Conversions of Datatypes

---
+++ 01_basics/03_converionOperations.js
@@ -0,0 +1,27 @@
+let score = "Chirag"; // 55, 55zyn, null, undefined, true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "55" => 55
+// "55xyn" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "chirag" => true
+
+let someNumber = 15091992
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
