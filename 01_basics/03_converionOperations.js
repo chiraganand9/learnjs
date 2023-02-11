f0c8650ca134bff01b661d39abc3f48502ab07db
Chirag Anand <chiraganand@live.com>
2023-02-11 22:32:05 +0000

    Operations

--- 01_basics/03_converionOperations.js
+++ 01_basics/03_converionOperations.js
@@ -23,5 +23,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 15091992
 
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+// ++++++++++++++++++++++ Operations ++++++++++++++++++
+
+let value = 9
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Chirag"
+let str3 = str1+str2;
+
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+/* Learn more about ToPrimitive from ECMAscript docs and HC english video*/
+//console.log((3 + 4) *5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1=num2=num3 = 2+2;
+
+let gameCounter=100; 
+++gameCounter;  //learn prefix and postfix from MDN Docs
+console.log(gameCounter);
